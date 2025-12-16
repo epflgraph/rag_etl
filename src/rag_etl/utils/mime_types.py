2bb4b147cdbca33d461d0d360c6243c0894a03c9
Aitor Pérez <aitor.perez@epfl.ch>
2025-12-16 13:50:35 +0100

    :sparkles: add default mime types

--- src/rag_etl/utils/mime_types.py
+++ src/rag_etl/utils/mime_types.py
@@ -1,12 +1,20 @@
 import mimetypes
 
 MARKDOWN = "text/markdown"
+TXT = "text/plain"
 PDF = "application/pdf"
 ZIP = "application/zip"
 IPYNB = "application/x-ipynb+json"
 MP4 = "video/mp4"
 
 mimetypes.add_type(IPYNB, ".ipynb")
+
+DEFAULT_MIME_TYPES = [
+    TXT,
+    MARKDOWN,
+    PDF,
+    IPYNB,
+]
 
 
 def guess_mime_type(path):

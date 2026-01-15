ab34adaa8c77c93bdbc281b1c765698ca990a591
Aitor Pérez <aitor.perez@epfl.ch>
2026-01-15 15:54:44 +0100

    :sparkles: add ragtest 2 course

--- src/rag_etl/courses/__init__.py
+++ src/rag_etl/courses/__init__.py
@@ -1,9 +1,11 @@
 from rag_etl.courses.common.base_course import BaseCourse
 from rag_etl.courses.com309 import COM309Course
 from rag_etl.courses.ragtest import RAGTESTCourse
+from rag_etl.courses.ragtest2 import RAGTEST2Course
 
 __all__ = [
     "BaseCourse",
     "COM309Course",
     "RAGTESTCourse",
+    "RAGTEST2Course",
 ]

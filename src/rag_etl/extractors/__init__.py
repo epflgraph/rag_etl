66afa71725afdc3cac99caf2edd3861fb111e3d0
Aitor Pérez <aitor.perez@epfl.ch>
2026-02-05 09:21:09 +0100

    :art: improvements in moodle extractor

--- src/rag_etl/extractors/__init__.py
+++ src/rag_etl/extractors/__init__.py
@@ -1,7 +1,9 @@
 from rag_etl.extractors.base_extractor import BaseExtractor
 from rag_etl.extractors.moodle import MoodleExtractor
+from rag_etl.extractors.local_folder import LocalFolderExtractor
 
 __all__ = [
     "BaseExtractor",
     "MoodleExtractor",
+    "LocalFolderExtractor",
 ]

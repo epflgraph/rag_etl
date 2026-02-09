d1e95fe0d22a48a45a8f8af1fad26acdd6f62439
Aitor Pérez <aitor.perez@epfl.ch>
2026-02-09 09:55:01 +0100

    :sparkles: add math115a course

--- src/rag_etl/courses/__init__.py
+++ src/rag_etl/courses/__init__.py
@@ -10,6 +10,7 @@ from rag_etl.courses.micro315 import MICRO315Course
 from rag_etl.courses.cs112g import CS112GCourse
 from rag_etl.courses.cs470 import CS470Course
 from rag_etl.courses.mgt645 import MGT645Course
+from rag_etl.courses.math115a import MATH115aCourse
 
 __all__ = [
     "BaseCourse",
@@ -24,4 +25,5 @@ __all__ = [
     "CS112GCourse",
     "CS470Course",
     "MGT645Course",
+    "MATH115aCourse",
 ]
---
+++ src/rag_etl/courses/math115a/__init__.py
@@ -0,0 +1,5 @@
+from rag_etl.courses.math115a.math115a_course import MATH115aCourse
+
+__all__ = [
+    "MATH115aCourse",
+]

cb2c7df00a8de3841b9a3e8bcaea0c55f436eccd
jllp-epfl <142986442+jllp-epfl@users.noreply.github.com>
2026-08-29 23:33:23 +0200

    feat: import new extractors/resources/transformers

--- src/rag_etl/extractors/__init__.py
+++ src/rag_etl/extractors/__init__.py
@@ -3,6 +3,7 @@ from rag_etl.extractors.moodle import MoodleExtractor
 from rag_etl.extractors.mooc import MOOCExtractor
 from rag_etl.extractors.ed_discussion import EdDiscussionExtractor
 from rag_etl.extractors.local_folder import LocalFolderExtractor
+from rag_etl.extractors.mediaspace import MediaspaceExtractor
 
 __all__ = [
     "BaseExtractor",
@@ -10,4 +11,5 @@ __all__ = [
     "MOOCExtractor",
     "EdDiscussionExtractor",
     "LocalFolderExtractor",
+    "MediaspaceExtractor",
 ]

e954d2f719dcaf7c6135a3de15592c691e224006
jllp-epfl <142986442+jllp-epfl@users.noreply.github.com>
2026-09-03 21:16:57 +0200

    fix: increase min distance frames to 90s

--- src/rag_etl/transformers/video_to_frames/video_to_frames_transformer.py
+++ src/rag_etl/transformers/video_to_frames/video_to_frames_transformer.py
@@ -35,7 +35,7 @@ class VideoToFramesTransformer(BaseTransformer):
     def __init__(
         self,
         type_subtypes=None,
-        min_slide_seconds: int = 10,
+        min_slide_seconds: int = 90,
         frame_offset_seconds: int = 5,
         language: str | None = None,
         mediaspace_url: str | None = None,
